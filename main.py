8efde7a3aae99ac494352e952c42ddfa1d3022df
0Maan0 <maan.scipio@student.uva.nl>
2025-03-23 18:23:00 +0100

    comment

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+"""
+University: University of Amsterdam
+Course: Scientific Computing
+Authors: Margarita Petrova, Maan Scipio, Pjotr Piet
+ID's: 15794717, 15899039, 12714933
+
+Description: Combines all the different modules into one and runs the required
+parts and plots.
+
+Typical usage example:
+
+python3 main.py
+"""
 from src.membrane_solver import MembraneSolver
 import matplotlib.pyplot as plt
 import numpy as np
